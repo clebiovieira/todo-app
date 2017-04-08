b73f70922e47e7d172dc6d6ed9c53793bbcd6260
Clebio <clebiovieira@gmail.com>
2017-04-07 21:12:21 -0300

    Aula 46 - Rotas e HashHistory

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -4,17 +4,16 @@ import 'modules/font-awesome/css/font-awesome.min.css'
 //import '../template/custom.css'
 
 import React from 'react'
-import Todo from '../todo/todo'
-import About from '../about/about'
+//import Todo from '../todo/todo'
+//import About from '../about/about'
 import Menu from '../template/menu'
-//import Routes from './routes'
+import Routes from './routes'
 
 //Esta entre parentes pq significa uma expresao que sera retornada
 //pela Arrow Function
 export default props => (
     <div className='container'>
         <Menu/>
-        <Todo/>
-        <About/>
+        <Routes/>  
     </div>
 )
