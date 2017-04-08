1501cfb1b9cf255080310b606e552efdbf1ca7ec
Clebio <clebiovieira@gmail.com>
2017-04-07 21:06:13 -0300

    Aula 45 - Menu Bootstrap

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -6,13 +6,14 @@ import 'modules/font-awesome/css/font-awesome.min.css'
 import React from 'react'
 import Todo from '../todo/todo'
 import About from '../about/about'
-//import Menu from '../template/menu'
+import Menu from '../template/menu'
 //import Routes from './routes'
 
 //Esta entre parentes pq significa uma expresao que sera retornada
 //pela Arrow Function
 export default props => (
     <div className='container'>
+        <Menu/>
         <Todo/>
         <About/>
     </div>
