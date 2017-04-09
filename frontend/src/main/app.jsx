d8645d98d0eeff6b183e560fb479ae783074d335
Clebio <clebiovieira@gmail.com>
2017-04-09 17:12:07 -0300

    Aula 57

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,7 +1,7 @@
 //"modules" foi o apelido dado no webpack.config.js para representar a pasta node_module
 import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
-//import '../template/custom.css'
+import '../template/custom.css'
 
 import React from 'react'
 //import Todo from '../todo/todo'
